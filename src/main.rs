d74bdbf643524d347056f52d664153362ce6ce25
Ryan Gerard Wilson <ryangerardwilson@gmail.com>
2024-05-28 18:04:02 +0530

    sync

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,7 @@ async fn main() {
     }
 
     if std::env::args().any(|arg| arg == "--version") {
-        print_insight("csvbro 1.1.7");
+        print_insight("csvbro 1.1.8");
         std::process::exit(0);
     }
 
