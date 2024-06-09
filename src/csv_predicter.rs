ad9e1572da1cfd1dc7ba633fa82dbbe045bb794b
Ryan Gerard Wilson <ryangerardwilson@gmail.com>
2024-06-09 18:24:09 +0530

    sync

--- src/csv_predicter.rs
+++ src/csv_predicter.rs
@@ -51,10 +51,10 @@ pub async fn handle_predict(
         "xgb_device": "",
         "xgb_cv": "",
         "xgb_interaction_constraints": "",
-        "hyperparameter_optimization_attempts": "",
-        "hyperparameter_optimization_result_display_limit": "",
-        "dask_workers": "",
-        "dask_threads_per_worker": ""
+        "hyperparameter_optimization_attempts": "7",
+        "hyperparameter_optimization_result_display_limit": "7",
+        "dask_workers": "4",
+        "dask_threads_per_worker": "1"
     }
 }
 
